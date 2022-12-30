f06996f7d0ff49029ce24b345620c2bc17b3c1a6
soygagi <101847092+soygagi@users.noreply.github.com>
2022-12-29 19:36:04 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 function setup(){
     canvas = createCanvas(640,420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Estatus: detectando objectos";
 }
+status = "";
 img = "";
 function preload(){
     img = loadImage('dog_cat.jpg');
@@ -13,4 +16,20 @@ function draw(){
     noFill();
     stroke("#DF0101");
     rect(30, 60, 450, 350);
+    fill("#FF00000");
+    text("gato", 320,120);
+    noFill();
+    stroke("FF00000");
+    rect(300,100,300,300);
+}
+function modelLoaded(){
+    console.log("Modelo cargado");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error, result){
+    if(error){
+        console.log(error);
+    }
+    console.log(result);
 }
